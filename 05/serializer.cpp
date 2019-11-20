d4b45bb52afbb5bfadcff94cd732983dbc4d544c
RobertGaz <43959043+RobertGaz@users.noreply.github.com>
2019-11-20 17:23:11 +0300

    Update serializer.cpp

--- 05/serializer.cpp
+++ 05/serializer.cpp
@@ -1,25 +1,69 @@
 #include "serializer.h"
 
 Serializer::Serializer(std::ostream& out_stream) : out(out_stream) {}
-Error Serializer::process(bool val) {
-    if (val) out << "true" << separator;
-    else out << "false" << separator;
-    
+
+Error Serializer::serialize_value(uint64_t val) {
+    out<<val<<separator;
     return NoError;
 }
+    
+Error Serializer::serialize_value(bool val) {
+	if (val) out << "true" << separator;
+	else out << "false" << separator;
+    
+	return NoError;
+}
+
+Error Serializer::process() 
+{
+	return NoError;
+}
+
+   
  
 Deserializer::Deserializer(std::istream& in_stream) : in(in_stream) {} 
 
-void Deserializer::flush_buf() {
-    for (unsigned i = 0; i < bufsize; ++i) 
-        buf[i] = '\0';
-}     
+Error Deserializer::deserialize_value(bool& val) 
+{
+	buf = "";
+	std::getline(in, buf, separator);
+            
+    if (buf == "")
+        return CorruptedArchive;
+    if (buf == "true")
+        val = true;
+    else if (buf == "false")
+        val = false;
+    else 
+        return CorruptedArchive;
+        
+    return NoError;
+}
     
+Error Deserializer::deserialize_value(uint64_t& val) 
+{	
+	buf = "";
+	std::getline(in, buf, separator);
+        
+    unsigned num;
+    uint64_t res = 0;
+                
+    for (size_t i = 0; i < buf.size(); ++i) {
+        if (buf[i] < '0' or buf[i] > '9')
+            return CorruptedArchive;
+            
+        num = buf[i] - '0';
+        res *= 10;
+        res += num;
+    }
+        
+    val = res;
+        
+    return NoError;
+}
+ 
     
 Error Deserializer::process() {
-    in.getline(&buf[0], bufsize, separator);
-    if (std::string(&buf[0]) != "")
-        return CorruptedArchive;
     return NoError;
 }
     
@@ -29,4 +73,5 @@ void Data::print() {
     std::cout << "c: " << c << std::endl; 
 }
     
+
 
