7406fed5f2990cf4edaf0e2aab5ffcb2c2dd0a84
RobertGaz <43959043+RobertGaz@users.noreply.github.com>
2019-11-20 22:19:53 +0300

    Update format.h

--- 06/format.h
+++ 06/format.h
@@ -12,47 +12,43 @@ const string err_str_2("arg num > args amnt");
 
 void get_args(vector<string>& vec);
 
-
 template <class T, class ... Types>
-void get_args(vector<string>& vec, T arg, Types ... args)
-{	
-	stringstream s;
-	s << arg;
-	vec.push_back(s.str());
-	get_args(vec, args...);
+void get_args(vector<string>& vec, const T& arg, const Types& ... args)
+{
+    stringstream s;
+    s << arg;
+    vec.push_back(s.str());
+    get_args(vec, args...);
 }
 
-size_t get_num(string str, string::iterator& it);
-		
+size_t get_num(const string& str, string::const_iterator& it);
 
 template <class ... Types>
-string format(string main_str, Types ... args) 
+string format(const string& main_str, const Types& ... args) 
 {
-	vector <string> vec;
-	get_args(vec, args...);	
-	
-	string res_str;
-	string::iterator it = main_str.begin();
-	size_t arg_num;
-	
-	while (it < main_str.end()) {
-	
-		if (*it == '}') {
-			throw runtime_error(err_str_1);
-		} else if (*it == '{') {
-			arg_num = get_num(main_str, it);
-			if (arg_num >= vec.size())	
-				throw runtime_error(err_str_2);
-				
-			res_str.append(vec[arg_num]);
-			
-		} else {
-			res_str.append(1, *it);
-			++it;
-		}
-	}
-	
-	return res_str; 
-}
+    vector <string> vec;
+    get_args(vec, args...);
 
+    string res_str;
+    string::const_iterator it = main_str.cbegin();
+    size_t arg_num;
+
+    while (it < main_str.cend()) {
+        if (*it == '}') {
+            throw runtime_error(err_str_1);
+        } else if (*it == '{') {
+            arg_num = get_num(main_str, it);
+            if (arg_num >= vec.size())
+                throw runtime_error(err_str_2);
+
+            res_str.append(vec[arg_num]);
+                
+        } else {
+            res_str.append(1, *it);
+            ++it;
+        }
+    }
+
+    return res_str; 
+}
 
