12f85dc76c8ee0e4e90f36a2df55cd64344e0edb
RobertGaz <43959043+RobertGaz@users.noreply.github.com>
2019-11-20 22:20:25 +0300

    Update format.cpp

--- 06/format.cpp
+++ 06/format.cpp
@@ -2,23 +2,22 @@
 
 void get_args(vector<string>& vec) {}
 
-size_t get_num(string str, string::iterator& it) 
+size_t get_num(const string& str, string::const_iterator& it) 
 {
-	++it;
-	if (it == str.end())
-		throw runtime_error(err_str_1);
-		
-	size_t res = 0;
-	while (*it != '}') {
-		if (it == str.end() or *it < '0' or *it > '9') 
-			throw runtime_error(err_str_1);
-			
-		res *= 10;
-		res += (*it - '0');
-		++it;
-	}
-	++it;
-	
-	return res;
-}
+    ++it;
+    if (it == str.cend())
+        throw runtime_error(err_str_1);
 
+    size_t res = 0;
+	while (*it != '}') {
+        if (it == str.end() or *it < '0' or *it > '9') 
+    	    throw runtime_error(err_str_1);
+
+        res *= 10;
+        res += (*it - '0');
+        ++it;
+    }
+    ++it;
+
+    return res;
+}
